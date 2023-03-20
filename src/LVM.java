e5b84818cf81dbe33ffd9957c0489fbc40e761e9
BT_4N2_32 <sifata3@Nycstudents.net>
2023-03-20 12:29:43 -0400

    yg

---
+++ src/LVM.java
@@ -0,0 +1,9 @@
+public class LVM {
+    private String name;
+    private String UUID;
+
+    public LVM(String name) {
+        this.name = name;
+
+    }
+}
---
+++ src/PhysicalDrive.java
@@ -0,0 +1,2 @@
+public class PhysicalDrive {
+}
