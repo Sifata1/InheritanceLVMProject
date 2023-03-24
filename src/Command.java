a8a9b8b3cc743edd8221277c7af9bb7218735411
BT_4N2_32 <sifata3@nycstudents.net>
2023-03-24 12:23:08 -0400

    almost done with extendVG

--- src/Command.java
+++ src/Command.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Command {
     private ArrayList<PhysicalDrive> drives;
     private ArrayList<PhysicalVolume> pVs;
+    private ArrayList<VolumeGroup> vGs;
 
     public String installDrive(String name, int size) {
         boolean isInstalled = false;
@@ -30,7 +31,7 @@ public class Command {
     public String createPV(String name, PhysicalDrive d) {
         boolean isInstalled = false;
         for (int i = 0; i < pVs.size(); i++) {
-            if (drives.get(0).getName().equals(name)) {
+            if (pVs.get(0).getName().equals(name)) {
                 isInstalled = true;
             }
         }
@@ -38,13 +39,41 @@ public class Command {
         else {
             PhysicalVolume newPV = new PhysicalVolume(name, d);
             pVs.add(newPV);
-            return name + "installed";
+            return name + " installed";
         }
     }
 
     public void listPVs() {
-        for (int i = 0; i < pVs.size(); i++) System.out.println(pVs.get(i).getName() + ": " + "[" + pVs.get(i).getSize() + "] " + "[" + pVs.get(i).getUuid() + "]");
+        for (int i = 0; i < pVs.size(); i++)
+            System.out.println(pVs.get(i).getName() + ": " + "[" + pVs.get(i).getSize() + "] " + "[" + pVs.get(i).getUuid() + "]");
     }
 
+    public String createVG(String name, PhysicalVolume v) {
+        boolean isInstalled = false;
+        for (int i = 0; i < vGs.size(); i++) {
+            if (vGs.get(0).getName().equals(name)) {
+                isInstalled = true;
+            }
+        }
+        if (isInstalled) return "Error, that volume group already exists!";
+        else {
+            VolumeGroup newVG = new VolumeGroup(name, v);
+            vGs.add(newVG);
+            return name + " installed";
+        }
+    }
 
+    public void listVGs() {
+        for (int i = 0; i < vGs.size(); i++)
+            System.out.println(vGs.get(i).getName() + ": " + "[" + vGs.get(i).getSize() + "] " + "[" + vGs.get(i).getUuid() + "]");
+    }
+
+    public String extendVG(VolumeGroup vg, PhysicalVolume pv) {
+        boolean isExtended = false;
+        if (vg.getpVs().contains(pv)) {
+            return "Error, that volume group already contains " + pv.getName();
+        } else {
+            vg.getpVs().add()
+        }
+    }
 }
--- src/VolumeGroup.java
+++ src/VolumeGroup.java
@@ -3,8 +3,27 @@ import java.util.ArrayList;
 public class VolumeGroup extends LVM {
     private ArrayList<PhysicalVolume> pVs;
     private ArrayList<LogicalVolume> lVs;
+    private int size;
+    private String id;
+    private PhysicalVolume pv;
 
-    public VolumeGroup(String name) {
+    public int getSize() {
+        return size;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public ArrayList<PhysicalVolume> getpVs() {
+        return pVs;
+    }
+
+    public VolumeGroup(String name, PhysicalVolume v) {
         super(name);
+        this.size = v.getSize();
+        id = getUuid();
+        this.pv = v;
+        pVs.add(v);
     }
 }
